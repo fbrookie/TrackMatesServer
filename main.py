e9a0b903cee0456371502c539ddda5146dbacb04
shaolei95 <shaolei955@gmail.com>
2024-10-08 22:08:09 -0700

    test

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app = FastAPI()
 
 @app.get("/api/toy")
 def get_toy():
-    return {"message": "Hello from Google App Engine using Python 3.12!"}
+    return {"message": "Hello, Lei from Google App Engine using Python 3.12!"}
